467b689c146e99a593e8ac9b7faef83e475a4c96
parkhayarn <s0224y92@gmail.com>
2024-09-09 16:34:20 +0900

    bear-chat-sample

---
+++ chat-sample-master/src/test/java/com/ohgiraffers/websockettest/WebSocketTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.websockettest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
