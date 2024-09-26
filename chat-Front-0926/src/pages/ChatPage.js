17ed66fe38036d7a40b9fb99dd8c20a3a42fde38
parkhayarn <s0224y92@gmail.com>
2024-09-26 17:53:12 +0900

    ChatFrontTest

--- chat-Front-0926/src/pages/ChatPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ChatRoom from '../components/ChatRoom';
-
-const ChatPage = ({ match }) => {
-    const { roomId } = match.params;
-    return <ChatRoom roomId={roomId} />;
-};
-
-export default ChatPage;
